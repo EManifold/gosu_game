1f0925ce3d67726c25807ac7189855cd32bf6512
Elizabeth Manifold <elizabethmanifold@Elizabeths-MacBook-Pro.local>
2019-10-01 10:30:11 +0100

    add the ability to close the window

--- spec/lib/game_window_spec.rb
+++ spec/lib/game_window_spec.rb
@@ -18,6 +18,15 @@ describe GameWindow do
         expect(@game_window.height).to eq 600
     end
 
+    describe ".button_down" do
+        it "closes the window when the button pressed is Escape" do
+            game_window = GameWindow.new
+            escape_key = Gosu::KbEscape
+            expect_any_instance_of(GameWindow).to receive(:close)
+            game_window.button_down(escape_key)
+        end
+    end
+
     end
     describe ".name" do
         it "returns a default caption" do
